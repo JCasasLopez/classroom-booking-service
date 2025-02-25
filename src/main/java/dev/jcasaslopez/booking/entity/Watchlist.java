c9dd999af2bc90d1faf8f9972ded6b86265cb8e3
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-02-25 12:25:27 +0100

    Add Watchlist entity

---
+++ src/main/java/dev/jcasaslopez/booking/entity/Watchlist.java
@@ -0,0 +1,85 @@
+package dev.jcasaslopez.booking.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="watchlist_alerts")
+public class Watchlist {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long idWatchlist;
+	private int idClassroom;
+	private int idUser;
+	private LocalDateTime start;
+	private LocalDateTime finish;
+	private LocalDateTime timestamp;
+	
+	public Watchlist(long idWatchlist, int idClassroom, int idUser, LocalDateTime start, LocalDateTime finish) {
+		this.idWatchlist = idWatchlist;
+		this.idClassroom = idClassroom;
+		this.idUser = idUser;
+		this.start = start;
+		this.finish = finish;
+		this.timestamp = LocalDateTime.now();
+	}
+
+	public Watchlist() {
+		super();
+	}
+
+	public long getIdWatchlist() {
+		return idWatchlist;
+	}
+
+	public void setIdWatchlist(long idWatchlist) {
+		this.idWatchlist = idWatchlist;
+	}
+
+	public int getIdClassroom() {
+		return idClassroom;
+	}
+
+	public void setIdClassroom(int idClassroom) {
+		this.idClassroom = idClassroom;
+	}
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getFinish() {
+		return finish;
+	}
+
+	public void setFinish(LocalDateTime finish) {
+		this.finish = finish;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+}
