1dc4dd014f82aa70e3f68ca8eca6f094172b219c
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-11 18:57:23 +0100

    Feat(SlotManager): add interface methods for checking classroom availability and opening hours

--- src/main/java/dev/jcasaslopez/booking/slot/SlotManager.java
+++ src/main/java/dev/jcasaslopez/booking/slot/SlotManager.java
@@ -12,4 +12,14 @@ public interface SlotManager {
 	// Creates an availability calendar for a classroom within a specified time period.
 	List<SlotDto> createCalendar(int idClassroom, LocalDateTime start, LocalDateTime finish);
 	
+	// Verifica que las aulas estén abiertas durante el período de tiempo especificado.
+	//
+	// Checks classrooms are open during the specified period of time.
+	boolean classroomsOpenDuringPeriod(LocalDateTime start, LocalDateTime finish);
+	
+	// Verifica que no haya reservas para un aula durante el período de tiempo especificado.
+	//
+	// Checks there are no bookings for a classroom during the specified period of time.
+	boolean classroomAvailableDuringPeriod(int idClassroom, LocalDateTime start, LocalDateTime finish);
+	
 }
