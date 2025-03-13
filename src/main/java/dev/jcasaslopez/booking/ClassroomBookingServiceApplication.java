14d6c7b7577974a912eb7161bb4f1e4387ef6fc6
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-13 19:47:00 +0100

    Chore(ClassroomBookingServiceApplication): add @EnableScheduling

--- src/main/java/dev/jcasaslopez/booking/ClassroomBookingServiceApplication.java
+++ src/main/java/dev/jcasaslopez/booking/ClassroomBookingServiceApplication.java
@@ -2,8 +2,10 @@ package dev.jcasaslopez.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ClassroomBookingServiceApplication {
 
 	public static void main(String[] args) {
