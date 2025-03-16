3b688b5f2daf1971d840cb594dbdbfd542a20f48
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-16 13:44:51 +0100

    Feat(WatchAlertRepository): add support for WatchAlertService methods

--- src/main/java/dev/jcasaslopez/booking/repository/WatchAlertRepository.java
+++ src/main/java/dev/jcasaslopez/booking/repository/WatchAlertRepository.java
@@ -1,9 +1,20 @@
 package dev.jcasaslopez.booking.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import dev.jcasaslopez.booking.entity.WatchAlert;
 
 public interface WatchAlertRepository extends JpaRepository<WatchAlert, Long> {
-
+	
+	@Query("SELECT w FROM WatchAlert w WHERE w.idUser = :idUser")
+	List<WatchAlert> findWatchAlertsByUser(int idUser);
+	
+    @Query("SELECT w FROM WatchAlert w WHERE w.idClassroom = :idClassroom AND w.start >= :start "
+    		+ "AND w.finish <= :finish")
+	List<WatchAlert> findWatchAlertsByTimePeriodAndClassroom(int idClassroom, LocalDateTime start, 
+			LocalDateTime finish);
 }
