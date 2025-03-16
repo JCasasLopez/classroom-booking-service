268f19c21a8a65ea1c5ae6d0a155b0391dde903c
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-16 13:23:22 +0100

    Refactor: delete multiple files when renaming WatchAlert

--- src/main/java/dev/jcasaslopez/booking/repository/WatchlistRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.jcasaslopez.booking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.jcasaslopez.booking.entity.Watchlist;
-
-public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
-
-}
