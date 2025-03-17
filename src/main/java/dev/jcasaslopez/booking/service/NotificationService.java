1d2c933dec6410c411b8ef6e9f214fa4468014ee
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-17 19:17:58 +0100

    Fix(NotificationService): change sendNotification() signature

--- src/main/java/dev/jcasaslopez/booking/service/NotificationService.java
+++ src/main/java/dev/jcasaslopez/booking/service/NotificationService.java
@@ -1,12 +1,15 @@
 package dev.jcasaslopez.booking.service;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 import dev.jcasaslopez.booking.enums.NotificationType;
 
 public interface NotificationService {
 
-	List<String> messageBuilder(NotificationType notificationType);
-	void sendNotification();
+	Map<String, String> messageBuilder(NotificationType notificationType, int idUser, int idClassroom,
+			LocalDateTime start, LocalDateTime finish);
+	void sendNotification(NotificationType notificationType, int idUser, int idClassroom,
+			LocalDateTime start, LocalDateTime finish);
 	
 }
