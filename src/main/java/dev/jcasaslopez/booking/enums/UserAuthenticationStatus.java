cf323fa01bd29a85fd54d1eb3a1b04ef7f0c2beb
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-03-18 19:58:09 +0100

    Chore: add UserAuthenticationStatus enumeration

---
+++ src/main/java/dev/jcasaslopez/booking/enums/UserAuthenticationStatus.java
@@ -0,0 +1,11 @@
+package dev.jcasaslopez.booking.enums;
+
+public enum UserAuthenticationStatus {
+	//  Token inválido o expirado
+	//  Invalid or expired token
+	FAILED_AUTHENTICATION,
+
+	//	Usuario autenticado 
+	//	User is authenticated 
+	USER_AUTHENTICATED        	
+}
